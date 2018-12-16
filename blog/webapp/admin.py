d25a8639f8a2c1ed085f574d48110980456ace25
belek <belektolubaev@gmail.com>
2018-12-16 13:44:20 +0600

    added to admin.py filter

--- blog/webapp/admin.py
+++ blog/webapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from webapp.models import User, Article, Comment, Rating
 
+class UserAdmin(admin.ModelAdmin):
+    filter_horizontal = ('favorites',)
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Article)
 admin.site.register(Comment)
 admin.site.register(Rating)
